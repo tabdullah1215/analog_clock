0375da454b5ee2ce6c3fbc9a83845d9f42206c77
tabdullah1215 <tabdullah1215@live.com>
2017-08-13 03:21:35 -0400

    seconds hand functionality

--- src/CanvasComponent.js
+++ src/CanvasComponent.js
@@ -8,18 +8,44 @@ export default class CanvasComponent extends Component {
     constructor(props){
         super(props);
         this.state={
-
+            theta: -Math.PI/2
         }
     }
     componentDidMount(){
-        this.updateCanvas();
+        const {width, height} = this.props;
+        this.drawClock();
+        let interval = setInterval(() =>{
+            this.updateTime();
+        }, 1000);
     }
     updateCanvas(){
+        this.clearCanvas();
         this.drawClock();
         this.drawSecondHand();
     }
     updateTime(){
+        const {theta} = this.state;
+        let newTheta;
+        const inc = (1/60) * 2*Math.PI;
+        // if(theta + 0.1 > 2*Math.PI){
+        //     newTheta = (theta - 2*Math.PI) + inc;
+        // }
+        //else {
+            newTheta = theta + inc;
+        //}
 
+        this.setState({
+            theta: newTheta
+        });
+    }
+    clearCanvas(){
+        let {theta, height, width} = this.props;
+        const ctx = this.refs.canvas.getContext("2d");
+        const offset = height / 2;
+        ctx.translate(-offset, -offset);
+        ctx.beginPath();
+        ctx.clearRect(0, 0, width, height);
+        ctx.closePath();
     }
 
     drawClock(){
@@ -34,22 +60,21 @@ export default class CanvasComponent extends Component {
         ctx.fill();
         ctx.lineWidth = 4;
         ctx.stroke();
-        // ctx.beginPath();
-        // ctx.moveTo(0,0);
-        // ctx.lineTo(300,150);
 
-        //for(let theta = 0; theta < 2*Math.PI; theta += 0.1) {
-
-        //}
     }
     drawSecondHand(){
-        let {theta, height, width, origin} = this.props;
+        let {initialTheta, height, width, origin} = this.props;
+        let {theta} = this.state;
         const ctx = this.refs.canvas.getContext("2d");
+
         const handLength = (height/2) * 0.70;
         ctx.moveTo(origin.x, origin.y);
         ctx.lineTo(handLength * Math.cos(theta), handLength * Math.sin(theta));
         ctx.strokeStyle = "red";
         ctx.stroke();
+    }
+    componentDidUpdate(){
+        this.updateCanvas();
     }
     render(){
         const {width, height} = this.props;
@@ -62,8 +87,5 @@ export default class CanvasComponent extends Component {
 CanvasComponent.defaultProps = {
     width: 300,
     height: 300,
-    origin: {x: 0, y: 0},
-    theta: -Math.PI/2
-
-
+    origin: {x: 0, y: 0}
 };
